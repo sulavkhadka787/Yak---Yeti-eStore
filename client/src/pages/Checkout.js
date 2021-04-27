1f970ad4e1903944503bf45dca35c9228b0256b3
sulavkhadka787 <sulav.khadka787@gmail.com>
2021-04-26 21:32:59 -0600

    final order summary

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
+import React, { useEffect,useState } from 'react';
+import {useSelector,useDispatch} from 'react-redux';
 import {Link} from 'react-router-dom';
 import Nav from '../components/nav';
+import {getUserCart} from '../functions/user';
 
 const Checkout=()=>{
+
+    const [products,setProducts]=useState([]);
+    const [total,setTotal]=useState(0);
+
+    const {cart,user}=useSelector((state)=>({...state}));
+    const dispatch=useDispatch();
+
+    useEffect(()=>{
+        console.log('checkout-useeffect');
+        getUserCart(user.token).then((res)=>{
+            console.log('user cart',JSON.stringify(res.data,null,4));
+            setProducts(res.data.products);
+            setTotal(res.data.cartTotal);
+        })
+    },[]);
+
 
     const saveAddressToDb=()=>{
         //
@@ -13,10 +30,12 @@ const Checkout=()=>{
         //
     }
 
-    const {cart,user}=useSelector((state)=>({...state}));
+    
 
     const getTotal=()=>{
-        //
+        return products.reduce((currentValue,nextValue)=>{
+            return currentValue+nextValue.count*nextValue.price
+        },0)
     }
 
     return(
@@ -24,6 +43,7 @@ const Checkout=()=>{
         <Nav/>
             <div className="cart-container">
                 <div>
+                {JSON.stringify(products)}
                     <button onClick={saveAddressToDb}>Save address to db</button>
                 </div>
             <div className="order-summary">
@@ -39,11 +59,11 @@ const Checkout=()=>{
                         </thead>
                         <tbody>
 
-                        {cart.map((c,i)=>(
+                        {products.map((p,i)=>(
                         <tr key={i}>
-                                <td>{c.title.substring(0,15)} </td>
-                                <td>{c.count}</td>
-                                <td>${c.count * c.price}</td>
+                                <td>{p.product.title.substring(0,15)} </td>
+                                <td>{p.count}</td>
+                                <td>${p.count * p.product.price}</td>
                         </tr>
                     
                     ))}
